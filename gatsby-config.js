17abe2a5e2cbf160fe0ead8d1bb4565b37440e85
Aleph Retamal <7674479+alaphao@users.noreply.github.com>
2021-01-08 16:09:52 -0300

    build: configure eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,17 @@ module.exports = {
         allExtensions: true, // defaults to false
       },
     },
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
   ],
 }
